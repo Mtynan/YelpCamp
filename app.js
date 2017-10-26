e27a828438764f79d3f8b0a0a03c755ebf2186a9
Matt <Mtynan4@gmail.com>
2017-10-26 22:44:33 +0100

    Added Bootstrap, Header/Footer, ejs

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res){
+    res.render("index")
+})
+
+
+
+app.listen("3000", function(req, res){
+    console.log("Running on Port 3000");
+});
