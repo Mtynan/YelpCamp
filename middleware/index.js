10633767dcca161f8d2b8f9816d41d9403f24637
Matt <Mtynan4@gmail.com>
2017-10-28 19:40:18 +0100

    updated landing page

--- middleware/index.js
+++ middleware/index.js
@@ -11,11 +11,13 @@ middlewareObj.checkCampgroundOwnership = function (req, res, next){
                 if(foundCampground.author.id.equals(req.user._id)){
                     next();
                 } else {
+                    req.flash("error", "You don't have permission to do that");
                     res.redirect("back");
                }               
             }
         });
     } else {
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 };
@@ -24,16 +26,19 @@ middlewareObj.checkCommentOwnership = function (req, res, next){
     if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id, function(err, foundComment){
             if(err){
+                req.flash("error", "Campground not found");
                 res.redirect("back")
             } else {
                 if(foundComment.author.id.equals(req.user._id)){
                     next();
                 } else {
+                    req.flash("error", "You don't have permission to do that");
                     res.redirect("back");
                }               
             }
         });
     } else {
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 }
